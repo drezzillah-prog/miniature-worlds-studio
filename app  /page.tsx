83abb61b9e97253f17486f2458a823d11779fcfa
drezzillah-prog <drezzillah@gmail.com>
2026-06-01 12:22:02 +0300

    Update page.tsx

--- app  /page.tsx
+++ app  /page.tsx
@@ -1,1 +1,17 @@
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import FeaturedWorld from "@/components/FeaturedWorld";
+import CollectionsGrid from "@/components/CollectionsGrid";
+import Footer from "@/components/Footer";
 
+export default function Home() {
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <FeaturedWorld />
+      <CollectionsGrid />
+      <Footer />
+    </main>
+  );
+}
